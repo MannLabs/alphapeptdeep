49f5a9a3ae3c544f11c47d5b116b78ad38a1b72d
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2022-04-28 13:58:02 +0200

    add citation

--- peptdeep/model/rt.py
+++ peptdeep/model/rt.py
@@ -24,9 +24,7 @@ mod_feature_size = len(model_const['mod_elements'])
 
 #export
 
-# If there are copyright problems for these peptides,
-# we can easily choose different peptides those have the same
-# predicted retention time.
+
 IRT_PEPTIDE_DF = pd.DataFrame(
     [['LGGNEQVTR', 'RT-pep a', -24.92, '', ''],
     ['GAGSSEPVTGLDAK', 'RT-pep b', 0.00, '', ''],
@@ -192,14 +190,14 @@ class AlphaRTModel(model_interface.ModelInterface):
         precursor_df: pd.DataFrame
     ):
         self.predict(precursor_df)
-    # simple linear regression
+        # simple linear regression
         rt_pred_mean = precursor_df.rt_pred.mean()
         irt_mean = precursor_df.irt.mean()
         x = precursor_df.rt_pred.values - rt_pred_mean
         y = precursor_df.irt.values - irt_mean
-    slope = np.sum(x*y)/np.sum(x*x)
-    intercept = irt_mean - slope*rt_pred_mean
-    # end linear regression
+        slope = np.sum(x*y)/np.sum(x*x)
+        intercept = irt_mean - slope*rt_pred_mean
+        # end linear regression
         precursor_df['irt_pred'] = precursor_df.rt_pred*slope + intercept
         return precursor_df
 
