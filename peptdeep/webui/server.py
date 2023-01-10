0d61803f0cffa67a3b49e346aa07906f49158b6e
Zeng, Wen-Feng <jalew188@gmail.com>
2023-01-11 00:31:27 +0100

    serve process spawn for CUDA

--- peptdeep/webui/server.py
+++ peptdeep/webui/server.py
@@ -1,8 +1,6 @@
-from concurrent.futures import process
-from alphabase.yaml_utils import load_yaml
-
 import time
 import os
+import sys
 import shutil
 import torch.multiprocessing as mp
 
@@ -104,7 +102,10 @@ class PeptDeepServer:
     def start(self):
         if self.process is None:
             # self.process = mp.get_context('spawn').Process(target=serve)
-            self.process = mp.Process(target=serve)
+            if sys.platform.lower().startswith('linux'):
+                self.process = mp.get_context('spawn').Process(target=serve)
+            else:
+                self.process = mp.Process(target=serve)
             self.process.start()
 
             with open(self._process_file, 'w') as f:
