b62b62c7a13c92dc8b344444eee81fb60fb425c2
jalew188 <jalew188@gmail.com>
2023-12-26 16:37:05 +0100

    Bump version: 1.1.0 → 1.1.1

--- peptdeep/__init__.py
+++ peptdeep/__init__.py
@@ -11,7 +11,7 @@
 #     pass
 
 __project__ = "peptdeep"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __license__ = "Apache 2.0"
 __description__ = "The AlphaX deep learning framework for Proteomics"
 __author__ = "Mann Labs"
