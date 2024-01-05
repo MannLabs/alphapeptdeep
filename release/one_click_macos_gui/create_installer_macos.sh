1decdf7377b6b421f8cea61e05f90da2d89970ef
jalew188 <jalew188@gmail.com>
2024-01-05 22:17:33 +0100

    Bump version: 1.1.1 → 1.1.2

--- release/one_click_macos_gui/create_installer_macos.sh
+++ release/one_click_macos_gui/create_installer_macos.sh
@@ -20,7 +20,7 @@ python setup.py sdist bdist_wheel
 
 # Setting up the local package
 cd release/one_click_macos_gui
-pip install "../../dist/peptdeep-1.1.1-py3-none-any.whl[stable]"
+pip install "../../dist/peptdeep-1.1.2-py3-none-any.whl[stable]"
 
 # Creating the stand-alone pyinstaller folder
 pip install pyinstaller
@@ -40,5 +40,5 @@ cp ../../LICENSE.txt Resources/LICENSE.txt
 cp ../logos/alpha_logo.png Resources/alpha_logo.png
 chmod 777 scripts/*
 
-pkgbuild --root dist/peptdeep --identifier de.mpg.biochem.peptdeep.app --version 1.1.1 --install-location /Applications/peptdeep.app --scripts scripts peptdeep.pkg
+pkgbuild --root dist/peptdeep --identifier de.mpg.biochem.peptdeep.app --version 1.1.2 --install-location /Applications/peptdeep.app --scripts scripts peptdeep.pkg
 productbuild --distribution distribution.xml --resources Resources --package-path peptdeep.pkg dist/peptdeep_gui_installer_macos.pkg
