eb4f1e7f7713de218d629acb0ba7b90ebb814c8b
jalew188 <jalew188@gmail.com>
2024-05-30 02:32:04 +0800

    bump version: 1.1.9 → 1.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = 'peptdeep'
 copyright = '2022, Mann Labs, MPIB'
 author = 'Mann Labs, MPIB'
 
-release = "1.1.9"
+release = "1.2.0"
 
 # -- General configuration ---------------------------------------------------
 
