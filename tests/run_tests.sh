7d903815157089597c9d0a8a7fccafb15dbdea95
jalew188 <jalew188@gmail.com>
2024-07-23 23:29:20 +0200

    #183 to make tests work

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,2 +1,6 @@
-INCLUDED_NBS=$(find ../nbs_tests -name "*.ipynb")
-python -m pytest --nbmake $(echo $INCLUDED_NBS)
+TEST_NBS=$(find ../nbs_tests -name "*.ipynb")
+TUTORIAL_NBS=$(find ../docs/tutorials -name "*.ipynb")
+
+ALL_NBS=$(echo $TEST_NBS$'\n'$TUTORIAL_NBS)
+
+python -m pytest --nbmake $(echo $ALL_NBS)
