ba16fdeba33ec757f458cf568fe4ecba5c5e24b7
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-08-22 13:19:34 +0200

    fix install paths

--- release/macos/build_pkg_macos.sh
+++ release/macos/build_pkg_macos.sh
@@ -19,13 +19,13 @@ BUILD_NAME="${PACKAGE_NAME}-${PACKAGE_VERSION}-${KERNEL}-${ARCH}"
 
 # Wrapping the pyinstaller folder in a .pkg package
 mkdir -p dist/peptdeep/Contents/Resources
-cp ../logos/alpha_logo.icns dist/peptdeep/Contents/Resources
+cp release/logos/alpha_logo.icns dist/peptdeep/Contents/Resources
 mv dist/peptdeep_gui dist/peptdeep/Contents/MacOS
-cp Info.plist dist/peptdeep/Contents
-cp peptdeep_terminal dist/peptdeep/Contents/MacOS
-cp ../../LICENSE.txt Resources/LICENSE.txt
-cp ../logos/alpha_logo.png Resources/alpha_logo.png
-chmod 777 scripts/*
+cp release/macos/Info.plist dist/peptdeep/Contents
+cp release/macos/peptdeep_terminal dist/peptdeep/Contents/MacOS
+cp LICENSE.txt Resources/LICENSE.txt
+cp logos/alpha_logo.png Resources/alpha_logo.png
+chmod 777 release/macos/scripts/*
 
-pkgbuild --root dist/${PACKAGE_NAME} --identifier de.mpg.biochem.${PACKAGE_NAME}.app --version 1.2.1 --install-location /Applications/${PACKAGE_NAME}.app --scripts scripts ${BUILD_NAME}.pkg
-productbuild --distribution distribution.xml --resources Resources --package-path ${BUILD_NAME}.pkg dist/${BUILD_NAME}.pkg
+pkgbuild --root dist/${PACKAGE_NAME} --identifier de.mpg.biochem.${PACKAGE_NAME}.app --version 1.2.1 --install-location /Applications/${PACKAGE_NAME}.app --scripts release/macos/scripts ${BUILD_NAME}.pkg
+productbuild --distribution release/macos/distribution.xml --resources Resources --package-path ${BUILD_NAME}.pkg dist/${BUILD_NAME}.pkg
