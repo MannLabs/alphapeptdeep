3febb87c0a028c31ee67d9849755a69bfe558464
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-08-26 16:22:14 +0200

    rename & add comments

--- release/windows/build_installer.ps1
+++
@@ -1,3 +0,0 @@
-# Wrapping the pyinstaller folder in a .exe package
-&  "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" .\release\windows\peptdeep_innoinstaller.iss
-# WARNING: this assumes a static location for innosetup
