c3d96c0573243b9341448295a6770e31630a8dac
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-08-27 15:16:13 +0200

    remove debug windows installer

--- release/pypi/install_pypi_wheel.sh
+++ release/pypi/install_pypi_wheel.sh
@@ -1,3 +1,4 @@
+# TODO remove with old release workflow
 conda create -n peptdeep_pip_test python=3.9 -y
 conda activate peptdeep_pip_test
 pip install "peptdeep[stable]"
--- release/windows/build_installer_windows.ps1
+++ release/windows/build_installer_windows.ps1
@@ -4,9 +4,6 @@
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./build
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./dist
 
-# temporary fix for the issue with windows installer getting stuck
-pip install numpy==2.0.1 altair==5.4.0 packaging==24.1 rich==13.7.1 idna==3.7 importlib_metadata==8.4.0 zipp==3.20.0
-
 # Creating the wheel
 python setup.py sdist bdist_wheel
 # Make sure you include the required extra packages and always use the stable or very-stable options!
