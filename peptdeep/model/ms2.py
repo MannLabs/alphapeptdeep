bb28368aecee926cca3bebd66f6d824303778917
Mohamed Sameh <mohamedsameh2210@gmail.com>
2025-03-27 09:51:03 +0100

    Revert "allow ignoring safety when predicting"
    This reverts commit b72a1b762cdfe325d3fc546e47143209f2d9b434.

--- peptdeep/model/ms2.py
+++ peptdeep/model/ms2.py
@@ -656,10 +656,9 @@ class pDeepModel(model_interface.ModelInterface):
         batch_size=1024,
         verbose=False,
         reference_frag_df=None,
-        allow_unsafe_predictions=False,
         **kwargs,
     ) -> pd.DataFrame:
-        if not self._safe_to_predict and not allow_unsafe_predictions:
+        if not self._safe_to_predict:
             raise ValueError(
                 f"The model is not safe to use for prediction. This might mean that the requested charged_frag_types {self.charged_frag_types} are not a subset of the charged_frag_types used to train the loaded pretrained model {self.model.supported_charged_frag_types}. Please choose a subset of the supported charged_frag_types or retrain the model with the requested charged_frag_types."
             )
