054c913e1611db7922ce6ac5897e39f90dde65ce
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2026-03-17 14:21:06 +0100

    fix

--- peptdeep/match/normal_dia.py
+++ peptdeep/match/normal_dia.py
@@ -4,7 +4,8 @@ import typing
 from collections import defaultdict
 
 import numpy as np
-from alphatims.alphatims_wrapper import convert_to_alphatims, remove_unused_peaks
+
+from peptdeep.alphatims_wrapper import convert_to_alphatims, remove_unused_peaks
 from alphatims.bruker import TimsTOF
 
 from alpharaw.ms_data_base import MSData_Base
